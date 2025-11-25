c2c68cf5407bba4acc2a082892015276fad1a326
MD-Sazzadur-Rahman-Yamin <mailme.yamin@gmail.com>
2025-11-25 21:52:43 +0600

    init mongodb and make parcels post api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const cors = require('cors')
+const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 3333;
@@ -9,7 +10,43 @@ const port = process.env.PORT || 3333;
 app.use(express.json());
 app.use(cors());
 
+//mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zzj1wzu.mongodb.net/?appName=Cluster0`;
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("zap-shift-db");
+    const parcelsColl = db.collection("parcels");
+
+    //parcel API
+    app.get("/parcels", async (req,res)=> {
+
+    });
+    app.post("/parcels", async (req,res)=>{
+        const parcel = req.body;
+        const result = await parcelsColl.insertOne(parcel);
+        res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
