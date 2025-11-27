0c433689d523d253e6ba2cc932b224b5d0f8b74e
MD-Sazzadur-Rahman-Yamin <mailme.yamin@gmail.com>
2025-11-27 21:23:28 +0600

    make parcels delete api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 3333;
@@ -35,13 +35,22 @@ async function run() {
       if (email) {
         query.senderEmail = email;
       }
-      const cursor = parcelsColl.find(query);
+      const option = { sort: { createAt: -1 } };
+      const cursor = parcelsColl.find(query, option);
       const result = await cursor.toArray();
       res.send(result);
     });
     app.post("/parcels", async (req, res) => {
       const parcel = req.body;
+      parcel.createAt = new Date();
       const result = await parcelsColl.insertOne(parcel);
+      res.send(result);
+    });
+
+    app.delete("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = parcelsColl.deleteOne(query);
       res.send(result);
     });
 
