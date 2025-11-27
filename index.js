c731a4719c65510222222272a2d1096edc622fbd
MD-Sazzadur-Rahman-Yamin <mailme.yamin@gmail.com>
2025-11-27 22:17:16 +0600

    added get parcels by id api

--- index.js
+++ index.js
@@ -40,6 +40,13 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+
+    app.get("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await parcelsColl.findOne(query);
+      res.send(result);
+    });
     app.post("/parcels", async (req, res) => {
       const parcel = req.body;
       parcel.createAt = new Date();
